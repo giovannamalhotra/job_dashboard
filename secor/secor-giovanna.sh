f755954abd525d8d993e9b06736170dc1bfe58b0
Ubuntu <ubuntu@ip-172-31-0-199.us-west-2.compute.internal>
2016-09-15 00:02:14 +0000

    Create Secor shell script to start Secor to consume and upload Kafka messages

---
+++ secor/secor-giovanna.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - — - - - - - - - - -
+# Script to execute "Secor" to consume and upload kafka messages to Amazon S3
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - — - - - - - - - - -
+
+cd /usr/local/secor/bin
+sudo java -ea -Dsecor_group=secor_backup -Dlog4j.configuration=log4j.prod.properties -Dconfig=secor.prod.backup.properties -cp secor-0.22-SNAPSHOT.jar:lib/* com.pinterest.secor.main.ConsumerMain
