b16ace2d7baa62ba15facd7a4ffeb0ff26aec866
Ubuntu <ubuntu@ip-172-31-0-199.us-west-2.compute.internal>
2016-09-22 05:59:40 +0000

    Update pyspark to save final data set to Elasticsearch

--- batch_pyspark/jobs_batch.py
+++ batch_pyspark/jobs_batch.py
@@ -5,8 +5,9 @@ from pyspark import SparkContext, SparkConf, StorageLevel
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from elasticsearch import Elasticsearch
+from datetime import datetime
 
-#----- Define constants --------
+#----- Define constants --------#
 
 #Spark
 #SPARK_MASTER = os.environ['SPARK_MASTER']
@@ -18,6 +19,7 @@ SPARK_MASTER = 'ec2-52-89-46-245.us-west-2.compute.amazonaws.com'
 ES_NODES = 'ec2-52-26-9-10.us-west-2.compute.amazonaws.com'
 ES_INDEX = 'dashboard'
 ES_TYPE = 'jobs'
+ES_RESOURCE = 'dashboard/jobs'
 
 print "ES_NODES:" + ES_NODES
 print "SPARK_MASTER:" + SPARK_MASTER
@@ -52,14 +54,49 @@ if __name__ == '__main__':
 
    # folder on HDFS and Amazon S3 to pull the data from
    #val diceFile = "hdfs://ec2-52-89-46-245.us-west-2.compute.amazonaws.com:9000/camus/exec/history/2016-09-16*"
-   #val diceFileS3 = "s3a://giovanna-insight/raw_logs/secor_backup/diceFeed/"
-      
-
+   diceFileS3 = "s3a://giovanna-insight/raw_logs/secor_backup/diceFeed/*"
+   indeedFileS3 = "s3a://giovanna-insight/raw_logs/secor_backup/indeedFeed/*"
+       
    # construct RDD[Sting]
-   indeedStaticFile = '{"snippet": "The <b>data</b> science team is the core of 6sense <b>engineering</b>. Our <b>data</b> scientists are not optimizing software; We’re growing quickly and want to expand our <b>data</b>...", "city": "San Francisco", "state": "CA", "date": "Mon, 18 Jul 2016 05:47:44 GMT", "url": "http://www.indeed.com/viewjob?jk=231996de16f8c0a0&qd=w-2ovI1tcnsfYPwo0sLkH9zbhbnue5uotyVhKGbVyKMWZG2Qcqk4kcnEktztFnFoBz9U92JvRWUwZzl66iD44zxoxgsbBlaVUvGtwJ4oKEZSI4CmH9UzR8aTbSGPezp5qbUCPeLOQ7Dzs0cmDHOdgA&indpubnum=9693529091171604&atk=1astii41qburm805", "country": "US", "formattedLocation": "San Francisco, CA", "jobtitle": "Data Scientist", "company": "6sense", "formattedLocationFull": "San Francisco, CA"}'
+   #indeedStaticFile = 'json1.json'
+   #diceStaticFile = 'json2.json'
 
-   diceStaticFile = '{"date": "2016-09-09", "jobTitle": "Data and Application Engineering Manager", "company": "EPE Innovations", "location": "Dallas, TX", "detailUrl": "http://www.dice.com/job/result/90955654/637266?src=19"}' +   
-   '{"date": "2016-09-09", "jobTitle": "Data and Application Engineering Manager", "company": "EPE Innovations", "location": "Dallas, TX", "detailUrl": "http://www.dice.com/job/result/90955654/637266?src=19"}'
+   indeedDF = sqlContext.read.json(indeedFileS3)
+   diceDF = sqlContext.read.json(diceFileS3)
 
-   indeedDF = sqlContext.read.json(indeedStaticFile)
-   diceDF = sqlContext.read.json(diceStaticFile)
+   indeedDF.printSchema()
+   diceDF.printSchema()
+
+   indeedDF.registerTempTable("indeedTBL")
+   diceDF.registerTempTable("diceTBL")
+
+   newIndeedDF = sqlContext.sql("SELECT jobtitle, company, url, formattedLocation as location, date, snippet FROM indeedTBL")
+   newDiceDF = sqlContext.sql("SELECT jobTitle as jobtitle, company, detailUrl as url, location, date , detailUrl as snippet FROM diceTBL")
+   newIndeedDF.printSchema()
+   newDiceDF.printSchema()
+
+   newIndeedDF.registerTempTable("newIndeedTBL")
+   newDiceDF.registerTempTable("newDiceTBL")
+
+   # Join both DF contents
+   combinedDF = sqlContext.sql("SELECT jobtitle, company, url, location, date, snippet FROM newDiceTBL UNION ALL SELECT jobtitle, company, url, location, date, snippet FROM newIndeedTBL")
+   combinedDF.registerTempTable("combinedTBL")
+
+   # Dedup rows
+   combinedDedupDF = sqlContext.sql("SELECT jobtitle, company, location, first(url) as url, first(date) as date, first(snippet) as snippet FROM combinedTBL GROUP BY jobtitle, company, location")   
+
+   combinedDedupDF.printSchema() 
+   #finalRDD = combinedDedupDF.rdd
+   #finalRDD.take(10)
+
+   es_conf = {'es.nodes': ES_NODES, 'es.resource': ES_RESOURCE, 'es.port' : '9200',  'es.batch.write.retry.count': '-1', 'es.batch.size.bytes': '0.05mb'}
+   finalRDD = combinedDedupDF.map(lambda row: ('key', row.asDict()))
+   finalRDD.saveAsNewAPIHadoopFile(path='-', \
+                                            outputFormatClass='org.elasticsearch.hadoop.mr.EsOutputFormat', \
+                                            keyClass='org.apache.hadoop.io.NullWritable', \
+                                            valueClass='org.elasticsearch.hadoop.mr.LinkedMapWritable', \
+                                            conf=es_conf)
+
+
+   #combinedDedupDF.rdd.saveToEs("dashboard/jobs")
+   #finalRDD = combinedDedupDF.rdd
