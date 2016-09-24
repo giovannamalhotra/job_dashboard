0d09ed6c59b6ae8c4567110eda31cd229ced7b36
Ubuntu <ubuntu@ip-172-31-0-199.us-west-2.compute.internal>
2016-09-24 21:34:47 +0000

    Add company type to dashboard index. Connect to kafka brokers to process tweets

---
+++ database/create_company_type.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - — - - - - - - - - -
+# Script to create company type for "dashboard" index in  Elasticsearch 
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - — - - - - - - - - -
+
+#curl -XPOST 'http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/' -d '{"mapping": {"company ": {"properties": {"companyname ": { "type ": "string" }}}}}'
+
+#curl http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/_stats/indexes\?pretty\=1
+
+curl -XGET 'http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/_mapping/company'
+
+
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c1" -d '{"company" : "LinkedIn"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c2" -d '{"company" : "Facebook"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c3" -d '{"company" : "Fitbit"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c4" -d '{"company" : "Yelp"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c5" -d '{"company" : "Slack"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c6" -d '{"company" : "Twitter"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c7" -d '{"company" : "Opendoor"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c8" -d '{"company" : "Grand Rounds"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c9" -d '{"company" : "Tamr"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c10" -d '{"company" : "Casetext"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c11" -d '{"company" : "Komodo Heath"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c12" -d '{"company" : "6sense"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c13" -d '{"company" : "Spire"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c14" -d '{"company" : "Zenysis"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c15" -d '{"company" : "Domino Data Labs"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c16" -d '{"company" : "The Voleon Group"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c17" -d '{"company" : "Tophatter"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c18" -d '{"company" : "Microsoft"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c19" -d '{"company" : "Oracle"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c20" -d '{"company" : "Apple"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c21" -d '{"company" : "Google"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c22" -d '{"company" : "Accenture"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c23" -d '{"company" : "Yahoo"}'
+#curl -XPOST "http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c24" -d '{"company" : "Cisco"}'
+
+curl -XGET 'http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/company/c24'
--- streaming/process_tweets_stream.py
+++ streaming/process_tweets_stream.py
@@ -19,16 +19,23 @@ if __name__ == "__main__":
 
     #kafka_machines = envir_vars.storage_cluster_ips
     #zkQuorum = ','.join([m + ':2181' for m in kafka_machines])
-    zookeeperServ = 'ec2-52-89-43-209.us-west-2.compute.amazonaws.com:2181'
+    #zookeeperServ = 'ec2-52-89-43-209.us-west-2.compute.amazonaws.com:2181'
 
-    kafkaStream = KafkaUtils.createStream(ssc, zookeeperServ, "GroupNameDoesntMatter", topics)
+    #kafkastream = KafkaUtils.createStream(ssc, zookeeperServ, "GroupNameDoesntMatter", topics)
     #lines = kafkaStream.map(lambda x: x[1])
     #lines = kafkaStream.map(lambda x: json.loads(x[1]))
-    lines = kafkaStream.map(lambda (k, v): json.loads(v))
+    #lines = kafkaStream.map(lambda (k, v): json.loads(v))
 
-    for x in kafkaStream.collect():
-       print x
+    brokers = "ec2-50-112-19-115.us-west-2.compute.amazonaws.com:9092,ec2-52-33-162-7.us-west-2.compute.amazonaws.com:9092,ec2-52-89-43-209.us-west-2.compute.amazonaws.com:9092" 
+    #kafkabrokers = {'metadata.broker.list' : 'ec2-50-112-19-115.us-west-2.compute.amazonaws.com:9092,ec2-52-33-162-7.us-west-2.compute.amazonaws.com:9092,ec2-52-89-43-209.us-west-2.compute.amazonaws.com:9092'}
+    #hashablebrokers = frozenset(kafkabrokers.items())
+    #kafkastream = KafkaUtils.createDirectStream(ssc, [topics], hashablebrokers)
+    kafkastream = KafkaUtils.createDirectStream(ssc, ['tweetsFeed'], {"metadata.broker.list": brokers})
 
+    lines = kafkastream.map(lambda x: x[1])
+    linesRDDCollection = lines.flatMap(lambda line: line.split(" "))
+
+    linesRDDCollection.foreachRDD(lambda rdd: rdd.take(10))
 
     ssc.start()
     ssc.awaitTermination()
