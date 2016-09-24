99c2a1a910063857bc92d25d8e4b14be85b67596
Giovanna <giovanna@Giovannas-MacBook-Pro.local>
2016-09-24 16:44:56 -0700

    Add more Elasticsearch host to Flask config

--- web/app/views.py
+++ web/app/views.py
@@ -5,8 +5,8 @@ import json
 
 INDEX_NAME = 'dashboard'
 TYPE_NAME = 'jobs'
-es = Elasticsearch(hosts = 'ec2-52-26-9-10.us-west-2.compute.amazonaws.com')
-
+#es = Elasticsearch(hosts = 'ec2-52-26-9-10.us-west-2.compute.amazonaws.com')
+es = Elasticsearch(hosts = ['ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200','ec2-54-68-213-131.us-west-2.compute.amazonaws.com:9200','ec2-52-43-52-129.us-west-2.compute.amazonaws.com:9200'])
 
 #@app.route('/')
 @app.route('/index')
@@ -19,10 +19,9 @@ def index():
   #return render_template("index.html", title = 'Home', user = user, mylist = mylist)
   
   #res = es.search(index="dashboard", body={'query': {'match': {'description': 'CIA'}}})
-  res = es.search(index = INDEX_NAME, size = 50, body={"query": {"match_all": {}}})
+  #res = es.search(index = INDEX_NAME, size = 50, body={"query": {"match_all": {}}})
+  res = es.search(index = INDEX_NAME, size = 500, body={"query": {"match_all": {}}})
   jsonRes =  json.dumps(res, indent=2)
 
   return render_template("index.html", jsonResult = jsonRes)
 
-def email():
-  return render_template("base.html") 
