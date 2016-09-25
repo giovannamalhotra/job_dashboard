e3dfc5d6c08e97df8bd5d11fe197f223028c7d85
Giovanna <giovanna@Giovannas-MacBook-Pro.local>
2016-09-24 20:22:14 -0700

    Call backend 'search' method from the template

---
+++ web/app/static/js/app.js
@@ -0,0 +1,39 @@
+/*
+Javascript for job dashboard app
+*/
+
+console.log("Hello from app.js")
+
+
+function displayResults(jsonResult) {
+	console.log(jsonResult)
+}
+
+
+function search() {
+
+    var jobtitle = $('#jobtitleInput').val();
+    var company = $('#companyInput').val();
+    var location = $('#locationInput').val();
+ 
+ 	var inputData = {'jobtitle': jobtitle, 'company': company, 'location': location};
+
+ 	console.log("input: " + input)
+
+
+    $.ajax({
+        url: '/search',
+        //data: $('form').serialize(),
+        contentType : 'application/json; charset=utf-8',
+        data : JSON.stringify(inputData),
+        type: 'POST',
+        success: function(response) {
+            console.log("Ajax response:" + response);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+
+}
--- web/app/views.py
+++ web/app/views.py
@@ -10,12 +10,9 @@ es = Elasticsearch(hosts = ['ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200
 
 #@app.route('/')
 @app.route('/index')
-#@app.route('/email')
-
 def index():
-  #return "Hello, World!"
-  user = { 'nickname': 'Giovanna' } #fake user
-  mylist = [1,2,3,4]
+  #user = { 'nickname': 'Giovanna' } #fake user
+  #mylist = [1,2,3,4]
   #return render_template("index.html", title = 'Home', user = user, mylist = mylist)
   
   #res = es.search(index="dashboard", body={'query': {'match': {'description': 'CIA'}}})
@@ -25,3 +22,11 @@ def index():
 
   return render_template("index.html", jsonResult = jsonRes)
 
+
+@app.route('/search', methods=['POST'])
+def search():
+    #user =  request.form['username'];
+    #password = request.form['password'];
+
+    req_json =  request.get_json();   
+    return json.dumps({'status':'OK','req_json':req_json});
