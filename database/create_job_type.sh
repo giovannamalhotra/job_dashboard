703dca252f6464941fe99840a6dc3354f6390a74
Ubuntu <ubuntu@ip-172-31-0-199.us-west-2.compute.internal>
2016-09-28 19:27:04 +0000

    Update shell script that creates Elasticsearch job type

--- database/create_job_type.sh
+++ database/create_job_type.sh
@@ -5,9 +5,9 @@
 #
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - — - - - - - - - - -
 
-curl -XPUT 'http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/_mapping/jobrec' -d '{ "jobrec": { "properties": {"jobtitle": { "type": "string" }, "company": { "type": "string" }, "url": { "type": "string" }, "location": { "type": "string" }, "snippet": { "type": "string" }, "day": { "type": "integer" }, "month": { "type": "integer"}, "year": { "type": "integer"},  "creationdate": { "type": "String" }, "origin": { "type": "string" }}}}'
+curl -XPUT 'http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/_mapping/jobposting' -d '{ "jobposting": { "properties": {"jobtitle": { "type": "string" }, "company": { "type": "string" }, "url": { "type": "string" }, "location": { "type": "string" }, "snippet": { "type": "string" }, "strday": { "type": "string" }, "strmonth": { "type": "string"}, "stryear": { "type": "string"},  "creationdate": { "type": "String" }, "origin": { "type": "string" }}}}'
 
 #curl http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/_stats/indexes\?pretty\=1
 
-curl -XGET 'http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/_mapping/job'
+curl -XGET 'http://ec2-52-26-9-10.us-west-2.compute.amazonaws.com:9200/dashboard/_mapping/jobposting'
 
