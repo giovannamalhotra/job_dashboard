16ca28bc2fdb44d345e6cada04f36d1547c21a8a
Giovanna <giovanna@Giovannas-MacBook-Pro.local>
2016-09-30 10:19:11 -0700

    remove previous search results if user clicks on search again

--- web/app/static/js/app.js
+++ web/app/static/js/app.js
@@ -21,21 +21,23 @@ function search() {
         type: 'POST',
         success: function(response) {
             //console.log("response typeof: " + typeof response)
-            
-	    //var responseObj = JSON.stringify(eval("(" + response + ")"));
+                
+            //var responseObj = JSON.stringify(eval("(" + response + ")"));
             var responseObj = JSON.parse(response);
             //console.log("responseObj:" + responseObj + ", responseObj typeof: " + typeof responseObj);
-	    //console.log("responseObj:" + JSON.stringify(responseObj))	
+            //console.log("responseObj:" + JSON.stringify(responseObj))	
 
             var jobsArray = responseObj.resJSON;
-	    var jobtitle = "";
+            var jobtitle = "";
             var company = "";
             var location = "";
             var date = "";
             var url = "";
             var snippet = "";
             console.log("jobsArray length:" + jobsArray.length )
-       
+           
+            $(".jobsResultsSection .jobRow").not(".jobRowTemplate").remove();
+
             for (var i=0; i<jobsArray.length; i++) {
 
                 jobtitle = jobsArray[i]._source.jobtitle;
@@ -46,7 +48,7 @@ function search() {
                 //date = jobsArray[i]._source.day.toString() + ' - ' +  jobsArray[i]._source.month.toString() + ' - ' + jobsArray[i]._source.year.toString()   
                 //console.log("row: " + i + ", jobtitle:" + jobtitle + ", company:" + company + ", location:" + location + ", date:" + date + ", url:" + url + ", snippet:" + snippet);
 
-		$jobRow = $(".jobsResultsSection .jobRowTemplate").clone();
+        	    $jobRow = $(".jobsResultsSection .jobRowTemplate").clone();
             	$jobRow.removeClass("jobRowTemplate");
                 $jobRow.find(".jobtitleCompanyCol").text(jobtitle + " at " + company); 
                 $jobRow.find(".snippetCol").text(snippet); 
