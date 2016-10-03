bdc687422e06ce96607d0bb7199086b779d7ab42
Giovanna <giovanna@Giovannas-MacBook-Pro.local>
2016-10-03 12:48:01 -0700

    Add match statements for each word in the input fields, so it is an AND match

--- web/app/views.py
+++ web/app/views.py
@@ -33,17 +33,42 @@ def search():
     else: 
 
         criteria_list = []
-        if req_json["jobtitle"].strip():
-            criteria = { "match": { "jobtitle": req_json["jobtitle"].strip() }}
-            criteria_list.append(criteria)
 
-        if req_json["company"].strip():
-            criteria = { "match": { "company": req_json["company"].strip() }}
-            criteria_list.append(criteria)
+        #if req_json["jobtitle"].strip():
+        #    criteria = { "match": { "jobtitle": req_json["jobtitle"].strip() }}
+        #    criteria_list.append(criteria)
 
-        if req_json["location"].strip():
-            criteria = { "match": { "location": req_json["location"].strip() }}
-            criteria_list.append(criteria)
+        #if req_json["company"].strip():
+        #    criteria = { "match": { "company": req_json["company"].strip() }}
+        #    criteria_list.append(criteria)
+
+        #if req_json["location"].strip():
+        #    criteria = { "match": { "location": req_json["location"].strip() }}
+        #    criteria_list.append(criteria)
+
+
+        # Trim input field content
+        jobtitle_input = req_json["jobtitle"].strip()
+        company_input = req_json["company"].strip()
+        location_input = req_json["location"].strip()
+
+        if jobtitle_input:
+            jobtitle_words = jobtitle_input.split(' ')
+            for word in jobtitle_words:
+              criteria = { "match": { "jobtitle": word }}
+              criteria_list.append(criteria)
+
+        if company_input:
+            company_words = company_input.split(' ')
+            for word in company_words:
+              criteria = { "match": { "company": word }}
+              criteria_list.append(criteria)
+
+        if location_input:
+            location_words = location_input.split(' ')
+            for word in location_words:
+              criteria = { "match": { "location": word }}
+              criteria_list.append(criteria)
 
 
         #res = es.search(index = INDEX_NAME, doc_type = TYPE_NAME, body={"query": { "bool": {  "should": criteria_list } } })
